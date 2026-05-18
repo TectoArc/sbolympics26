da235a155de0f8450349f9e1cc4ee4f4a813629d
TectoArc <89665821+TectoArc@users.noreply.github.com>
2026-05-18 04:39:47 +0300

    Update SportsRegister.py

--- pages/SportsRegister.py
+++ pages/SportsRegister.py
@@ -7,9 +7,9 @@ scope = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive"
 ]
-creds = Credentials.from_service_account_file(
-    "service.json",
-    scopes=scope)
+creds = Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=scope
 
 client = gspread.authorize(creds)
 
